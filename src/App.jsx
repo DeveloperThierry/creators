40c99a406c7800bfa6680d9abdc3e74e235138b2
developerthierry <developerthierry@gmail.com>
2026-03-28 18:46:49 +0000

    Implement Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
 import "./client.js"
+import ShowCreators from './pages/ShowCreators.jsx'
+import NotFound from './pages/NotFound.jsx'
+import EditCreator from './pages/EditCreator.jsx'
+import AddCreator from './pages/AddCreator.jsx'
+import ViewCreator from './pages/ViewCreator.jsx'
 function App() {
   return(
   <>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<ShowCreators/>}/>
+      <Route path="/edit/:id" element={<EditCreator/>}/>
+      <Route path="/add" element={<AddCreator/>}/>
+      <Route path="/:id" element={<ViewCreator/>}/>
+      <Route path="*" element={<NotFound/>}/>
     </Routes>
   </>
   )
