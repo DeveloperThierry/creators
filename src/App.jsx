1830929a4e1193f23af10adc947f83b36db3fd41
developerthierry <developerthierry@gmail.com>
2026-03-28 18:29:24 +0000

    Connect to Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import "./client.js"
 function App() {
   return(
   <>
